6742a102e0ba550b07d28770ef8f66095c3184b9
CowNation <CowNation.cow@gmail.com>
2019-04-29 09:30:50 -0500

    Create c_bitset.h

---
+++ c_bitset.h
@@ -0,0 +1,15 @@
+#include "i_bitset.h"
+
+class c_bitset : public bitset {
+  private:
+  char pGet() const;
+  public:
+  c_bitset() : bitset(8) {}
+  
+  operator char() const;
+  char operator=(const char& b);
+  bool operator==(const char& b);
+  friend std::ostream & operator<<(std::ostream & _stream, c_bitset const & mc);
+};
+
+c_bitset ASCII(char letter); // Convert character to c_bitset
