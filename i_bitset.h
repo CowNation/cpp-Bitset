0ab9f984c32d823b9a258e368e21baa0e8f0089a
CowNation <CowNation.cow@gmail.com>
2019-05-10 08:38:37 -0500

    Added size member & fixed indentation

--- i_bitset.h
+++ i_bitset.h
@@ -2,13 +2,14 @@
 
 class i_bitset : public bitset {
   private:
-  double pGet() const;
+	const int SIZE = 32;
+  	double pGet() const;
 	i_bitset Integer(double i);
   public:
-  i_bitset() : bitset(32){}
-  friend std::ostream & operator<<(std::ostream & _stream, i_bitset const & mc);
-  operator int() const;
-  double operator=(const double& b);
+  	i_bitset() : bitset(32){}
+  	friend std::ostream & operator<<(std::ostream & _stream, i_bitset const & mc);
+  	operator int() const;
+  	double operator=(const double& b);
 	bool operator==(const i_bitset& b);
 	bool operator==(const int& b);
 };
