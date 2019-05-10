aa9d62ab77347d0840f18bfa95a8b17a47241582
CowNation <CowNation.cow@gmail.com>
2019-05-10 08:16:01 -0500

    see desc
    added support for larger values & changed constructor

--- bitset.cpp
+++ bitset.cpp
@@ -1,13 +1,7 @@
 #include "bitset.h"
 
-int bitset::GetValue(int Index) const {
+double bitset::GetValue(int Index) const {
   return pow(2, Index);
-}
-bitset::bitset(int bits){
-  BitSet.clear();
-  for(int i = 0; i < bits; i++){
-    BitSet.push_back(false);
-  }
 }
 void bitset::setbit(int Index, bool Value){
   BitSet[Index] = Value;
