c7387113c3cc5edf70bf08ce3c524841a9db0d22
CowNation <CowNation.cow@gmail.com>
2019-08-29 07:44:01 -0500

    Update c_bitset.h

--- c_bitset.h
+++ c_bitset.h
@@ -10,5 +10,6 @@ class c_bitset : public bitset {
   char operator=(const char& b);
   bool operator==(const char& b);
   friend std::ostream & operator<<(std::ostream & _stream, c_bitset const & mc);
-	c_bitset ASCII(char letter); // Convert character to c_bitset
 };
+
+c_bitset ASCII(char letter); // Convert character to c_bitset
