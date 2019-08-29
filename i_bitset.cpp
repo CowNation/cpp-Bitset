8efac7ad1284bbd1b9ffb967d2101222992bd999
CowNation <CowNation.cow@gmail.com>
2019-08-29 07:44:21 -0500

    Update i_bitset.cpp

--- i_bitset.cpp
+++ i_bitset.cpp
@@ -2,7 +2,7 @@
 
 double i_bitset::pGet() const {
   double ret = 0;
-  for (int i = 0; i < BitSet.size(); i++)
+  for (int i = 0; i < sizeof(BitSet) / sizeof(bool); i++)
     ret += BitSet[i] * GetValue(i);
   return ret;
 }
@@ -24,16 +24,15 @@ void checkValue(double& iLetter, i_bitset& integer, double Num, int Index){
 i_bitset i_bitset::Integer(double i){
   i_bitset integer;
   while (i > 0){
-    for (int k = 1; k < SIZE+1; k++){
-      checkValue(i, integer, GetValue(SIZE - k), SIZE - k);
-    }
+		for (int k = 1; k < 33; k++)
+			checkValue(i, integer, GetValue(32 - k), 32 - k);
   }
   return integer;
 }
 
 double i_bitset::operator=(const double& b) {
   i_bitset integer = Integer(b);
-  for (int i = 0; i < SIZE; i++){
+  for (int i = 0; i < 32; i++){
     setbit(i, integer.getbit(i));
   }
   return b;
@@ -46,4 +45,3 @@ bool i_bitset::operator==(const i_bitset& b){
 bool i_bitset::operator==(const int& b){
   return b == pGet();
 }
-
