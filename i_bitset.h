2e8c6f2ec711ae69394281bf3f262bc32a3e9648
CowNation <CowNation.cow@gmail.com>
2019-08-29 07:44:55 -0500

    Update i_bitset.h

--- i_bitset.h
+++ i_bitset.h
@@ -2,14 +2,13 @@
 
 class i_bitset : public bitset {
   private:
-	const int SIZE = 32;
-  	double pGet() const;
-	i_bitset Integer(double i);
+  double pGet() const;
+  i_bitset Integer(double i);
   public:
-  	i_bitset() : bitset(32){}
-  	friend std::ostream & operator<<(std::ostream & _stream, i_bitset const & mc);
-  	operator int() const;
-  	double operator=(const double& b);
-	bool operator==(const i_bitset& b);
-	bool operator==(const int& b);
+  i_bitset() : bitset(32){}
+  friend std::ostream & operator<<(std::ostream & _stream, i_bitset const & mc);
+  operator int() const;
+  double operator=(const double& b);
+  bool operator==(const i_bitset& b);
+  bool operator==(const int& b);
 };
