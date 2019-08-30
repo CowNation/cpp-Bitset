6bc20343ef2acabcae239222fe2bd487f24c8f73
CowNation <CowNation.cow@gmail.com>
2019-08-30 07:39:43 -0500

    Moved BitSet array to public

--- bitset.h
+++ bitset.h
@@ -5,12 +5,12 @@
 
 class bitset {
   protected:
-  bool* BitSet;
   double GetValue(int Index) const;
   public:
+  bool* BitSet;
   bitset(int bits, bool initialValue = false){
-		BitSet = new bool[bits];
-	}
+    BitSet = new bool[bits];
+  }
   void setbit(int Index, bool Value);
   bool getbit(int Index);
   void print();
